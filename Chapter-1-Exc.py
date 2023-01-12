1e610f1dbafa3828ff0a23a01ef66f4fce71fe9e
Nikola <107634968+J-Agostino@users.noreply.github.com>
2023-01-12 20:17:17 -0300

    Add files via upload
    Disclaimer: The answers to these exercises are personal and can be wrong. If you find a mistake, please let me know. 
    Thanks, cheers.
    J.

---
+++ Chapter-1-Exc.py
@@ -0,0 +1,65 @@
+""" 
+Exercise 1: What is the function of the secondary memory in a computer?
+
+a) Execute all of the computation and logic of the program
+b) Retrieve web pages over the Internet
+c) Store information for the long term, even beyond a power cycle  <--
+d) Take input from the user
+"""
+
+""" 
+Exercise 2: What is a program?
+A program is a set of stored instructions inside a memory of a PDA
+"""
+
+""" 
+Exercise 3: What is the difference between a compiler and an interpreter?
+A compiler is the tool to convert a script, or more, into a executable program. 
+An interpreter is a "translator" between human language and programming language, such as Python.
+"""
+
+""""
+Exercise 4: Which of the following contains “machine code”?
+a) The Python interpreter
+b) The keyboard
+c) Python source file <--
+d) A word processing document
+"""
+
+""" 
+Exercise 5: What is wrong with the following code:
+
+>>> primt 'Hello world!'
+File "<stdin>", line 1
+  primt 'Hello world!'
+                     ^
+SyntaxError: invalid syntax
+>>>
+
+Syntax error, the right line would be print('Hello world!')
+"""
+
+"""
+Exercise 6: Where in the computer is a variable such as “x” stored after the following Python line finishes?
+
+x = 123
+
+a) Central processing unit
+b) Main Memory <--
+c) Secondary Memory
+d) Input Devices
+e) Output Devices
+"""
+
+"""
+Exercise 7: What will the following program print out:
+
+x = 43
+x = x + 1
+print(x)
+
+a) 43
+b) 44 <--
+c) x + 1
+d) Error because x = x + 1 is not possible mathematically
+"""
